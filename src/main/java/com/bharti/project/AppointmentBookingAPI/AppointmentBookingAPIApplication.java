ba04d509ce1ed06a6686aadf965ee1bf9ac37971
Bharti Kumari <bhartikri2580@gmail.com>
2025-11-24 13:50:53 +0530

    Final: Working version with Lombok + correct main class

--- src/main/java/com/bharti/project/AppointmentBookingAPI/AppointmentBookingAPIApplication.java
+++ src/main/java/com/bharti/project/AppointmentBookingAPI/AppointmentBookingAPIApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppointmentBookingApiApplication {
+public class AppointmentBookingAPIApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppointmentBookingApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentBookingAPIApplication.class, args);
+    }
 }
